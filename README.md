13a506dcc45407e2f3bd1165cba6b2a28c3cd096
Immaculate Matloga <Immaculate@explore-datascience.net>
2019-03-18 11:11:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#mypackage
+This library was created as an example of how to publish my own python package
+##building this package locally
+'python setup.py sdist'
+##installing this package from GitHub
+'pip install git+https://github.com/'
+##updating this package from GitHub
+'pip install --upgrade git+https://github.com/'
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA mypackage python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/<Nkamogeleng>/<mypackage>',
+    author='Immaculate Nkamogeleng',
+    author_email='kamo21.km@gmail.com'
+)
